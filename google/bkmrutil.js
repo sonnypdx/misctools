9f6ebe2ed94ea443403b1cc57f3bceb1d44169be
Sonny Nallamilli <sonnypdx@gmail.com>
2018-03-13 11:50:06 -0700

    Update bkmrutil.js
    Removed explicit for loops and for-each's and optimized with reduce method.

--- google/bkmrutil.js
+++ google/bkmrutil.js
@@ -1,6 +1,6 @@
 // Based on the bookmarklet being provided by Google to bookmark a webpage to one's google account
 (function (){
-var l=["title","h1","h2","h3","h4","h5","h6"],
+var l="title,h1,h2,h3,h4,h5,h6".split(','),
 STWD="about,above,after,again,against,because,been,before,being,below,between,both,could,does,doing,down,during,each,from,further,have,having,he'd,he'll,he's,here,here's,hers,herself,himself,how's,i,i'd,i'll,i'm,i've,into,it's,itself,let's,more,most,myself,once,only,other,ought,ours,ourselves,over,same,she'd,she'll,she's,should,some,such,than,that,that's,their,theirs,them,themselves,then,there,there's,these,they,they'd,they'll,they're,they've,this,those,through,under,until,very,we'd,we'll,we're,we've,were,what,what's,when,when's,where,where's,which,while,who's,whom,why's,with,would,you'd,you'll,you're,you've,your,yours,yourself,yourselves".split(','),
 d = document, af=Array.from,
 // get the text strings from all of the HTMLElements matching the given tag
@@ -9,30 +9,21 @@ return af(d.getElementsByTagName(tg)).map(e=>e.textContent.trim()).filter(s=>s.l
 },
 // get the title to be used for the given webpage while bookmarking
 gtTtl=function(){
-for(var i=0;i<l.length;i++){
-	var st=gSS(l[i]);
-	if(st.length>0)return st.sort((a,b)=>{return b.length-a.length;})[0];
-}
-return d.title;
+var ts=l.reduce((g,x)=>g.concat(gSS(x)),[]).sort((a,b)=>b.length-a.length);
+return ts.length?ts[0]:d.title;
 },
 // get the synopsis or summary of the given webpage while bookmarking
 gtSum=function(){
 var ss=gSS('p');
-for(var i=0;i<ss.length;i++){
-	if(ss[i].length>0)return ss[i];
-}
-return '';
+return ss.length? ss[0]:'';
 },
 // get the words contained in the strings of all headings that pass the filter criteria
 gWs=function(ht){
-var kw=[],
-ss=gSS(ht);
-ss.forEach(s=>kw=kw.concat(s.toLowerCase().split(' ').filter(s=>s.length>3&&STWD.indexOf(s)<0)));
-return kw;
+return gSS(ht).reduce((g,x)=>g.concat(x.toLowerCase().split(' ').filter(s=>s.length>3&&STWD.indexOf(s)<0)),[]);
 },
 // get the keywords list from the heading tags of the given webpage while bookmarking
 gtKwH=function(){
-var d1=l.reduce((g,t)=>g.concat(gWs(t)), []).reduce((g,x)=>{g.set(x,(g.get(x)||0)+1);return g;}, new Map());
+var d1=l.reduce((g,t)=>g.concat(gWs(t)), []).reduce((g,x)=>g.set(x,(g.get(x)||0)+1), new Map());
 return af(d1).sort((a,b)=>b[1]-a[1]).reduce((g,x)=>g+=x[0]+',','');
 };
 // open the bookmarking url in a new window, code that was obtained from the original google bookmarklet
