2b6530a3fb6b5730d80182f1a4f18b0400f24177
Sonny Nallamilli <surya.nallamilli@yapicentral.com>
2023-12-21 14:57:27 -0800

    Adding pending stuff

---
+++ bookmarklets/reader/widen.js
@@ -0,0 +1,2 @@
+// widens the reading layout
+((d,w)=>{const s=d.querySelector(".content").style,t=d.querySelector(".toolbar").style;t.marginLeft="0";s.width=`${w}px`;s.marginLeft=`-${w/3}px`})(document,1800);
